9a119d6b92c965f161c258d198ce87b9366743c2
Mark Samberg <mjsamber@ncsu.edu>
2015-07-04 21:12:50 -0400

    Initial commit

---
+++ filter.php
@@ -0,0 +1,59 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Filter converting URLs in the text to HTML links
+ *
+ * @package    filter
+ * @subpackage deeplinks
+ * @copyright  2015 Mark Samberg <mjsamber@ncsu.edu>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class filter_deeplinks extends moodle_text_filter {
+
+    /**
+     * Apply the filter to the text
+     *
+     * @see filter_manager::apply_filter_chain()
+     * @param string $text to be processed by the text
+     * @param array $options filter options
+     * @return string text after processing
+     */
+    public function filter($text, array $options = array()) {
+
+        // TODO: Remove any script and other tags which we do not wish to filter. It
+        // is unlikely that we'll find any suitable links within these areas so for
+        // now this part has been left unfinished.
+
+        $search = "[[[[[[[todo]]]]]]]";
+        $text = preg_replace_callback($search, array($this, 'callback'), $text);
+
+        return $text;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // internal implementation starts here
+    ////////////////////////////////////////////////////////////////////////////
+    private function callback(array $matches) {
+        $embed = '<i class="fa '.$matches[1].'"></i>';
+
+        return $embed;
+    }
+}
